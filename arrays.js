c6a79beb2a393143ae914d94fc6533173a1a2354
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-27 16:45:10 -0400

    Add loops exercise

---
+++ arrays.js
@@ -0,0 +1,15 @@
+// 1. Define two arrays:
+//  - evens filled with even numbers
+//  - odds filled odd numbers
+
+
+// 2. In the function body, using the spread operator:
+//  - return a new array numbers that contains the elements of arr1 and arr2
+function combineArrays(arr1, arr2) {
+
+
+  }
+
+  
+  // 3. Uncomment the code below to test the function.
+  // console.log(combineArrays(evens, odds));
---
+++ loops.js
@@ -0,0 +1,75 @@
+const colors = ["blue", "green", "white"];
+
+// 1.  Using a forEach loop:
+//  - Log every color from the colors array to the console.
+
+
+let nums = [35, 482, 201, 98];
+// 2. Using a forEach loop:
+//  - Find and print the sum of the nums array.
+
+const cats = ["siamese", "calico", "persian", "scottish fold"];
+// 3. Using a for of loop:
+//  - Print the first letter of each cat to the console.
+
+const primeNums = [2, 3, 5, 7, 11, 13, 17, 19, 23];
+// 4. Using a for of loop:
+//  - Calculate the square of every element in the primeNums array.
+
+// 5. Using a for loop:
+//  - Iterate from 0 to 15.
+//  - For each iteration, it should check:
+//    - If the current number is odd or even,
+//    - and print it to the console.
+//
+//  Sample output:
+//    "0 is even"
+//    "1 is odd"
+//    "2 is even"
+
+// 6. Using a for loop:
+//  - Iterate the integers from 1 to 15.
+//  - For multiples of three, print "Fizz" instead of the number
+//  - For multiples of five print "Buzz"
+//  - For numbers that are multiples of both three and five print "FizzBuzz".
+//
+//    Sample output:
+//     1
+//     2
+//     Fizz
+//     4
+//     Buzz
+//     Fizz
+//     7
+//     8
+//     Fizz
+//     Buzz
+//     11
+//     Fizz
+//     13
+//     14
+//     FizzBuzz
+//     -----
+
+// BONUS LOOPS!!
+// Write a JavaScript program to construct the following pattern,
+//    using a nested for loop.
+//
+//    Output:
+//     *
+//     * *
+//     * * *
+//     * * * *
+//     * * * * *
+
+// Write a function called doubleValues that takes an array
+//    as a parameter. The function should return a new array
+//    with all the values in the array doubled
+//
+//    Examples & Sample Output:
+//     doubleValues([1,2,3]) // [2,4,6]
+//     doubleValues([5,1,2,3,10]) // [10,2,4,6,20]
+//
+//    Uncomment the two arrays below to use and test out the function.
+//     const testArr1 = [7, 3, 2, 6, 0];
+//     const testArr2 = [5, 10, 25, 15, 45];
