4a1a2fbbbbbab066f6f40d187560b2834135034d
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-11-08 15:12:51 -0500

    Added forLoop.js & linked to index.html

---
+++ forLoops.js
@@ -0,0 +1,20 @@
+let fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+
+// 1. Use a for loop to display each fruit to the console.
+
+
+
+let horrorMovies = [
+    "The Exorcist",
+    "Halloween",
+    "A Nightmare on Elm Street",
+    "The Shining",
+    "The Texas Chain Saw Massacre",
+    "Night of the Living Dead"
+  ];
+// 2. Use a for loop to display the first three items in the horrorMovies array to the console..
+
+
+
+
+// 3. Can you create a loop that only does the last three in the array? Try it out.
